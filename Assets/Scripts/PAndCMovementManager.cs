6491b75076b58d00a5c9706cac2dbddc7f757b4b
tarazhong <zhong.tara@gmail.com>
2019-01-03 12:05:12 +0100

    Added P&C detection on the ground

---
+++ Assets/Scripts/PAndCMovementManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This script has to be attached in the (walking) collider sprite
+// A Polygin Collider 2D must also be attached to detect mouse click
+public class PAndCMovementManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseUp()
+    {
+        Debug.Log("in");
+    }
+
+    private void OnMouseDown()
+    {
+
+    }
+}
