dbe39c883308ba6295f38e5dcd8a0c03197af933
AlabamaBen <benjamin.b.morelle@gmail.com>
2019-01-16 16:53:04 +0100

    Add minigame 2

---
+++ Assets/Scripts/minigame_2/Bubble_Behaviour.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bubble_Behaviour : MonoBehaviour {
+
+    public Transform sprite; 
+
+    public TextMesh textmesh;
+
+    public string Text = "Pomme";
+
+    public float speed = 10f;
+
+    public Sheet_Behaviour sheet_Behaviour; 
+
+    Rigidbody2D rigidbody;
+
+	// Use this for initialization
+	void Start () {
+        sprite.localScale = new Vector2(0.2f * Text.Length, transform.localScale.y);
+        textmesh.text = Text;
+        rigidbody = GetComponent<Rigidbody2D>();
+
+        rigidbody.AddForce(new Vector2(speed * Random.value , speed * Random.value));
+        rigidbody.AddTorque(speed/2 * Random.Range(-1, 1));
+    }
+
+    // Update is called once per frame
+    void Update () {
+        textmesh.text = Text;
+        sprite.localScale = new Vector2(0.2f * Text.Length, transform.localScale.y);
+    }
+
+    private void OnMouseDown()
+    {
+        sheet_Behaviour.hit();
+        GameObject.Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/minigame_2/Sheet_Behaviour.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sheet_Behaviour : MonoBehaviour {
+
+    public float line_size = 0.5f;
+
+    public Transform mask;
+
+    public int score; 
+
+	// Use this for initialization
+	void Start () {
+        score = 0; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            hit(); 
+        }
+	}
+
+    public void hit()
+    {
+        mask.Translate(new Vector2(0, - line_size));
+    }
+}
