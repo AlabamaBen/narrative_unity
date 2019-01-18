31a19ce557dd216abc4c4f4cf935c99d3101c80f
AlabamaBen <benjamin.b.morelle@gmail.com>
2019-01-18 15:11:29 +0100

    edit sound

--- Assets/Scripts/Sound_Manager/SFXSound_Voice.cs
+++ Assets/Scripts/Sound_Manager/SFXSound_Voice.cs
@@ -6,17 +6,29 @@ public class SFXSound_Voice : MonoBehaviour {
 
     public AudioClip[] theSounds;
 
+    public int Talk_Rate = 2; 
+
     // Use this for initialization
     void Start () {
     }
 
+    private int count = 0 ; 
+
     public void PlayTheSound()
     {
-        if(!GetComponent<AudioSource>().isPlaying)
+
+        count++; 
+
+        if(count % Talk_Rate == 0)
         {
             int index = Random.Range(0, theSounds.Length - 1);
             GetComponent<AudioSource>().clip = theSounds[index];
             GetComponent<AudioSource>().Play();
         }
+
+        //if(!GetComponent<AudioSource>().isPlaying)
+        //{
+
+        ////}
     }
 }
