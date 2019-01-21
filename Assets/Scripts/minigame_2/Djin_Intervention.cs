7a484e4dabf40787eee7e39c25578283f9dbb5ee
AlabamaBen <benjamin.b.morelle@gmail.com>
2019-01-21 17:20:54 +0100

    Add textmeshpro

--- Assets/Scripts/minigame_2/Djin_Intervention.cs
+++ Assets/Scripts/minigame_2/Djin_Intervention.cs
@@ -1,16 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro; 
 
 public class Djin_Intervention : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    public TMPro.TextMeshPro boiteDialogue;
+    //public SFXSound_Voice talk_sound;
+    public float text_speed = 0.02f;
+
+    // Use this for initialization
+    void Start () {
+
+        StartCoroutine(AnimateTextMonolog("LOLOLOLLOLOLOLLOLOLOLLOLOLOLLOLOLOLLOLOLOL", text_speed));
+
+    }
+
+
+    public IEnumerator AnimateTextMonolog(string strComplete, float speed)
+    {
+        int i = 0;
+        //talk_sound.PlayTheSound();
+        while (i < strComplete.Length)
+        {
+            boiteDialogue.text += strComplete[i++];
+            yield return new WaitForSeconds(speed);
+        }
+    }
 }
