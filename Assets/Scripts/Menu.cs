c4250df072f68f3e731bbe8d2e116d6e29c2f11f
AlabamaBen <benjamin.b.morelle@gmail.com>
2019-01-22 00:45:34 +0100

    Add compressed sounds

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,18 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour {
 
+    public GameObject Credit;
+    public GameObject MainMenu;
+    public Image blackpanel;
+    public float fade_speed = 3f;
+
+    bool fadeout = false; 
 
     public void OnClickPlay()
     {
-        Debug.Log("PLAY"); 
+        Debug.Log("PLAY");
+
+        fadeout = true;
+        blackpanel.gameObject.SetActive(true);
+    }
+
+    private void Update()
+    {
+        if(fadeout)
+        {
+            blackpanel.color = new Color(blackpanel.color.r, blackpanel.color.g, blackpanel.color.b, blackpanel.color.a + fade_speed * Time.deltaTime);
+            if (blackpanel.color.a > 0.99f)
+            {
+                fadeout = false;
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
+        }
     }
 
     public void OnClickCredits()
     {
         Debug.Log("CREDITS");
+        MainMenu.SetActive(false);
+        Credit.SetActive(true);
+    }
+
+    public void OnClickExitCredits()
+    {
+        Debug.Log("CREDITS");
+        MainMenu.SetActive(true);
+        Credit.SetActive(false);
     }
 
 }
