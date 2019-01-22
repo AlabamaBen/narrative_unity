a77f00d80bfbf65617fbd23a155164c2926da58a
tarazhong <zhong.tara@gmail.com>
2019-01-22 20:57:44 +0100

    Imported new cinematique 2

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -41,8 +41,8 @@ public class GameManager : MonoBehaviour {
 
         // TEST
         //Cinematics.instance.DisplayCinematic(2);
-        step=17;
-        dialoguesSeqFinished = false;
+        //step=17;
+        //dialoguesSeqFinished = false;
         //ClickableObjetManager.phase = 1;
     }
 
