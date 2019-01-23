4dfa531d23d4ff085385d24375dc9b4380079347
tarazhong <zhong.tara@gmail.com>
2019-01-23 09:02:07 +0100

    assets import wip

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -51,7 +51,7 @@ public class GameManager : MonoBehaviour
         //step = 17;
         //dialoguesSeqFinished = false;
         //ClickableObjetManager.phase = 1;
-        step = 41;
+        //step = 41;
 
     }
 
@@ -129,7 +129,7 @@ public class GameManager : MonoBehaviour
                 if (m_Scene.name != "minigame_1" && !sceneLoaded)
                 {
                     //TEST, next line to remove 
-                    Ring.Game_End = true;
+                    //Ring.Game_End = true;
 
                     CurtainsFadeIn();
                     sceneLoaded = true;
@@ -498,7 +498,7 @@ public class GameManager : MonoBehaviour
                 if (m_Scene.name != "minigame_2" && !sceneLoaded)
                 {
                     //TEST, next line to remove
-                    Minigame2_Behavior.MINIGAME2_END = true;
+                    //Minigame2_Behavior.MINIGAME2_END = true;
 
                     CurtainsFadeIn();
                     sceneLoaded = true;
