6756c2b98e440bb81b0433f73eb30f114eb37c97
tarazhong <zhong.tara@gmail.com>
2019-01-23 04:05:45 +0100

    Almost finished game master

---
+++ Assets/Scripts/CardOnClick.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardOnClick : MonoBehaviour
+{
+    public int choix;
+    private static bool isClicked = false;
+    
+    private void OnMouseDown()
+    {
+        if (!isClicked)
+        {
+            isClicked = true;
+            GameManager.instance.ChoixCarte(choix);
+        }
+    }
+}
